543169bb3a10f075461a461b20d6757c8ce81487
Amanuel <35485646+amanuelpraisechris@users.noreply.github.com>
2025-08-31 17:18:11 +0800

    refactor: centralize nonPatientNameFields constant

--- src/utils/recordConstants.ts
+++ src/utils/recordConstants.ts
@@ -13,6 +13,17 @@ export const visitByOptions = [
 
 // Add missing export
 export const nonPatientNameFields = [
-  'id', 'patientId', 'birthDate', 'sex', 'address', 'phoneNumber', 
-  'email', 'village', 'subVillage', 'district', 'identifiers'
+  'id', 'patientId', 'birthDate', 'sex', 'address', 'phoneNumber',
+  'email', 'village', 'subVillage', 'district', 'identifiers',
+  'balozi_first_name', '"balozi_first_name"',
+  'balozi_middle_name', '"balozi_middle_name"',
+  'balozi_last_name', '"balozi_last_name"',
+  'oldest_member_first_name', '"oldest_member_first_name"',
+  'oldest_member_middle_name', '"oldest_member_middle_name"',
+  'oldest_member_last_name', '"oldest_member_last_name"',
+  'cellLeaderFirstName', 'cellLeaderMiddleName', 'cellLeaderLastName',
+  'oldestHouseholdMemberFirstName', 'oldestHouseholdMemberMiddleName', 'oldestHouseholdMemberLastName',
+  'Ten Cell Leader First Name', '"Ten Cell Leader First Name"',
+  'Ten Cell Leader Middle Name', '"Ten Cell Leader Middle Name"',
+  'Ten Cell Leader Last Name', '"Ten Cell Leader Last Name"'
 ];
